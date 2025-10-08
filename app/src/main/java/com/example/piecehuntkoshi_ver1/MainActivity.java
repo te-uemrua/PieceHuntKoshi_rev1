6e6eac9196be276547c62ddf8127718bc13cbb8e
rento <uemurento@gmail.com>
2025-10-08 14:21:18 +0900

    import文追加

--- app/src/main/java/com/example/piecehuntkoshi_ver1/MainActivity.java
+++ app/src/main/java/com/example/piecehuntkoshi_ver1/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.piecehuntkoshi_ver1; // あなたのパッケージ名に合わせてください
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolygonOptions;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * 地図を表示するためのメイン画面 (Activity)
