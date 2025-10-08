07e7f5cbe70dde2e512bff8434edcf60db77606d
yoshimoto <te22yoshimoto@g.kumamoto-nct.ac.jp>
2025-10-08 16:41:44 +0900

    初期レイアウト構築、機能インポート

--- app/src/main/java/com/example/piecehuntkoshi_ver1/shake_phone.java
+++ app/src/main/java/com/example/piecehuntkoshi_ver1/shake_phone.java
@@ -1,4 +1,12 @@
 package com.example.piecehuntkoshi_ver1;
 
-public class shake_phone {
+import android.app.Activity;    // 画面機能(Activity)のインポート
+import android.os.Bundle;    // 画面状態管理機能(Bundle)のインポート
+
+public class shake_phone extends Activity {    // Activityの継承
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {    // 最初に呼ばれる
+        super.onCreate(savedInstanceState);    // 親の初期化
+        setContentView(R.layout.shake_phone);    // XML画面の表示
+    }
 }
