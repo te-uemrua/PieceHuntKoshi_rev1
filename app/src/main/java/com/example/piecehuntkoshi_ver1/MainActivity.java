e1a17e93e3b5ce532a96df7dc2df6792d9cdfd6f
rento <uemurento@gmail.com>
2025-10-08 13:27:31 +0900

    目的地の範囲が分かるようにしたよ

--- app/src/main/java/com/example/piecehuntkoshi_ver1/MainActivity.java
+++ app/src/main/java/com/example/piecehuntkoshi_ver1/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.piecehuntkoshi_ver1; // あなたのパッケージ名に合
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Color;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -42,13 +44,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // --- ここから地図に対する操作を記述 ---
 
-        // 1. とりあえずの表示位置として「合志市役所」の緯度経度を指定
-        LatLng koshiCityHall = new LatLng(32.8849, 130.7513);
+        // 1. とりあえずの表示位置として「熊本県農業カントリーパーク」の緯度経度を指定
+        LatLng koshiCityHall = new LatLng(32.8900575, 130.7595619);
 
         // 2. その位置にマーカー（ピン）を立てる
-        mMap.addMarker(new MarkerOptions().position(koshiCityHall).title("合志市役所"));
+        mMap.addMarker(new MarkerOptions().position(koshiCityHall).title("熊本県農業カントリーパーク"));
 
         // 3. 指定した座標にカメラを移動させる（ズームレベルは15）
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(koshiCityHall, 15f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(koshiCityHall, 14f));
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center(koshiCityHall)  // 円の中心座標
+                .radius(500)           // 半径 (メートル単位)
+                .strokeColor(Color.RED) // 線の色 (赤)
+                .strokeWidth(5f)        // 線の太さ
+                .fillColor(0x55ff0000); // 塗りつぶしの色 (半透明の赤)
+
+        // 2. 地図に円を追加する
+        mMap.addCircle(circleOptions);
+
     }
 }
