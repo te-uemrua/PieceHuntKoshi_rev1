c851c6d1bad9183eae43160d1133bacc2dbdc458
hirakida satoshi <sa10shira@gmail.com>
2025-10-15 13:48:19 +0900

    room_databaseの依存関係を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //room_databaseのやつ
+    val room_version = "2.2.6"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 }
