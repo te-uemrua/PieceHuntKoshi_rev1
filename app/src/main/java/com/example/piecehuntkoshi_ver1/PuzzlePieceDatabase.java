16a3c47cc2be07250ed1cf0c600530f5263822ad
hirakida satoshi <sa10shira@gmail.com>
2025-10-22 11:13:28 +0900

    同じやつをインポートしていたので修正

--- app/src/main/java/com/example/piecehuntkoshi_ver1/PuzzlePieceDatabase.java
+++ app/src/main/java/com/example/piecehuntkoshi_ver1/PuzzlePieceDatabase.java
@@ -2,7 +2,6 @@ package com.example.piecehuntkoshi_ver1;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-import androidx.room.RoomDatabase;
 
 @Database(entities = {PuzzlePieceEntity.class}, version = 1)
 public abstract class PuzzlePieceDatabase extends RoomDatabase{
